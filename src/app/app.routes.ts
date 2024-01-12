4fbe9be4e383fae0607b1c6332be1a778f0a9b0f
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-01-12 13:19:13 +0100

    hh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import {HomeComponent} from './home/home.component'
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'admin', component: AdminComponent},
+  {path:'home', component: HomeComponent},
+  {path:'blogPage', component: BlogPageComponent},
+  // Redirect empty path to '/home'
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // Wildcard for 404 - Page Not Found
+  { path: '**', component: PageNotFoundComponent }
+];
